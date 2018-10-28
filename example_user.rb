be162d9ff2e6be09417bb3c5e3b3aaf3ae04e548
椿野彩 <tsubakino@logiclogic.jp>
2018-10-29 03:37:59 +0900

    example_user.rb

---
+++ example_user.rb
@@ -0,0 +1,22 @@
+class User
+  attr_accessor :name, :email
+
+  def initialize(attributes = {})
+   # @name  = attributes[:name]
+    @first_name = attributes[:first_name]
+    @last_name = attributes[:last_name]
+    #@email = attributes[:email]
+  end
+
+  def formatted_email
+    "#{@name} <#{@email}>"
+  end
+
+  def full_name
+  "#{@first_name} #{@last_name}"
+  end
+
+  def alphabetical_name
+    full_name.split(' ').join(',')
+  end
+end
