0013844b9f11ee940fc12739dc24badfb8251209
椿野彩 <tsubakino@backys-MacBook-Pro.local>
2019-07-31 14:33:45 -0400

    fixpath

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'contacts/new'
-  get 'contacts/create'
+  post 'contacts/create'
 
   get 'password_resets/new'
   get 'password_resets/edit'
@@ -8,7 +8,6 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get '/help',    to: 'static_pages#help'
   get '/about',   to: 'static_pages#about'
-  get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
   get    '/login',   to: 'sessions#new'
